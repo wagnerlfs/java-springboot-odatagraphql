7f0ca6fe2089e1f37d6096d6a3cb91f4f7b216c1
wagnerlfs <wagnerlfs@gmail.com>
2024-05-13 17:10:16 -0300

    Atualizando as informações do Projeto referente integração via ODATA e GraphQL

---
+++ src/main/java/br/com/mrs/integracao/odatagraphql/OdatagraphqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrs.integracao.odatagraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdatagraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdatagraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mrs/integracao/odatagraphql/OdatagraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrs.integracao.odatagraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdatagraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
